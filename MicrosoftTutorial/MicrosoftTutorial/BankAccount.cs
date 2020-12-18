dbd108aaae9e008d6aa94a36fde45b87420422b0
Maricar Walters <walterskai7@gmail.com>
2020-12-18 13:51:57 -0600

    initial commit for microsoft c# classes tutorial part 1

---
+++ MicrosoftTutorial/MicrosoftTutorial/BankAccount.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicrosoftTutorial
+{
+    class BankAccount
+    {
+        private static int accountNumberSeed = 1234567890;
+        public string AccountNumber { get; set; }
+        public decimal Balance 
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+            //set
+            //{
+            //    Balance += value;
+            //}
+        }
+        public string Owner { get; set; }
+
+        
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.AccountNumber = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+        }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal.");
+            }
+            var withdrawal = new Transaction(amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+    }
+}
---
+++ MicrosoftTutorial/MicrosoftTutorial/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MicrosoftTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
+
+            var account = new BankAccount("Louie", 88599.70M);
+            Console.WriteLine($"Account {account.AccountNumber} has been created for {account.Owner} with {account.Balance} initial balance.");
+            
+            account.MakeWithdrawal(1500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+
+            account.MakeDeposit(2000, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
